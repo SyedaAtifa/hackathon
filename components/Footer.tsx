4ab6ca64db6417c9dc1545b820a73ac1521389e6
Syeda Atifa <syedaatifa4307@gmail.com>
2024-12-11 19:35:13 +0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,16 +3,16 @@ import Image from 'next/image'
 
 function Footer() {
   return (
-    <div className="w-screen h-[505px] border-t font-poppins xsm:h-[1000px]">
+    <div className="w-screen h-[505px] border-t font-poppins xsm:h-[1200px]">
         <footer className="w-[1240px] h-[419px] flex flex-col items-center justify-center xsm:w-screen">
-            <div className="w-full h-[312px] flex gap-20 ml-20 xsm:flex-col">
+            <div className="w-full h-[312px] flex gap-20 ml-20 xsm:flex-col xsm:w-screen xsm:ml-1">
                 <div className="w-[285px] h-[100px]">
                     <h2 className="text-2xl font-bold">Funiro.</h2>
                     <p className="text-[#9F9F9F] mt-16">400 University Drive Suite 200 Coral Gables,
                     FL 33134 USA</p>
                 </div>
 
-                <div className="w-[710px] h-[312px] flex gap-20  xsm:w-screen xsm:h-auto">
+                <div className="w-[710px] h-[312px] flex gap-20 xsm:flex-col xsm:w-screen xsm:h-auto">
                     <ul className="w-[68px] h-[312px] flex flex-col justify-evenly font-medium">
                         <li className="text-[#9F9F9F]">Links</li>
                         <li>Home</li>
@@ -33,7 +33,7 @@ function Footer() {
                     </div>
                 </div>
             </div>
-            <div className="w-full h-[59px] border-t flex ml-12">
+            <div className="w-full h-[59px] border-t flex ml-12 absolute top-[6700px] xsm:w-screen xsm:ml-0">
                 <p className="mt-12">2023 furino. All rights reverved</p>
             </div>
         </footer>
