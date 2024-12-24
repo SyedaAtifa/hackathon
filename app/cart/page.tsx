aee92ac74b79e26ee05370588a7d911147717b26
Syeda Atifa <syedaatifa4307@gmail.com>
2024-12-24 18:01:22 +0500

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -25,6 +25,31 @@ function page() {
                         <li>Subtotal</li>
                     </ul>
                 </div>
+                <div className="w-full mt-16 flex gap-10">
+                    <Image src={"/images/cartimage.svg"} alt='cart' width={108} height={105} />
+                    <div className="w-[573px] mt-12 flex justify-between text-[#9F9F9F]">
+                      <p>Asgaard sofa</p>
+                      <span>Rs. 250,000.00</span>
+                      <span className="h-8 w-8 border rounded-[5px] flex items-center justify-center">1</span>
+                      <p className="text-black">Rs. 250,000.00</p>
+                    </div>
+                    <div className="w-7 h-7 mt-12">
+                      <Image src={"/images/del.svg"} alt='delete' width={21} height={21}/>
+                    </div>
+                </div>
+            </div>
+
+           <div className="w-[393px] h-[390px] border bg-[#F9F1E7] flex flex-col items-center gap-10">
+                <h6 className="h-12 my-7 text-[32px] font-semibold">Cart Totals</h6>
+                <p className="font-medium flex gap-12">Subtotal 
+                    <span className="text-[#9F9F9F]">Rs. 250,000.00</span>
+                </p>
+                <p className="font-medium flex gap-20">Total
+                    <span className="text-[#B88E2F]">Rs. 250,000.00</span>
+                </p>
+                <Link href={"/checkout"}>
+                    <button className="w-[222px] h-[59px] border border-black rounded-[15px] text-xl">Checkout</button>
+                </Link>
             </div>
         </div>
 
